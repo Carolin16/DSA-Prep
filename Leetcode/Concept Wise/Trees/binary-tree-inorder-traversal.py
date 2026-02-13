d4963bb9f86dbcf7c6580df4a58cfea5c209d7b6
Carolin16 <62839685+Carolin16@users.noreply.github.com>
2026-02-12 21:39:05 -0600

    Create binary-tree-inorder-traversal.py

---
+++ Leetcode/Concept Wise/Trees/binary-tree-inorder-traversal.py
@@ -0,0 +1,32 @@
+"""
+  Q.94 https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+Time Complexity : O(n) Every node is visited exactly once. 
+It is "pushed" onto the stack once and "popped" once.
+
+Space Complexity : O(h) The stack stores the path from the root to the current node. h is the height of the tree.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        res = []
+        curr = root
+        while curr is not None or len(stack) > 0:
+            while curr is not None:
+                stack.append(curr)
+                curr = curr.left
+                
+            
+            curr = stack.pop()
+            res.append(curr.val)
+
+            curr = curr.right
+        
+        return res
+        
