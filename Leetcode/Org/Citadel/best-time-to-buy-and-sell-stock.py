01549f82735475877972e72c0cf0165ce6745b9d
Carolin16 <62839685+Carolin16@users.noreply.github.com>
2026-02-28 19:41:39 -0600

    Create best-time-to-buy-and-sell-stock.py

---
+++ Leetcode/Org/Citadel/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_price = prices[0]
+        max_price = 0
+
+        for price in prices:
+            max_price = max(max_price , price - min_price)
+            min_price = min(min_price,price)
+
+        return max_price
+        
